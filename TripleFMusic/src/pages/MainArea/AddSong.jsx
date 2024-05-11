ddf0ccd9bf33bc47093db82a0183950e144bbac5
FAES <faes.extra@gmail.com>
2024-05-11 21:40:22 +0200

    Kleinere Anpassungen + automatisch erstellte ReadMe entfernt und nur unsere gelassen

--- TripleFMusic/src/pages/MainArea/AddSong.jsx
+++ TripleFMusic/src/pages/MainArea/AddSong.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function AddSong() {
   return (
-    <div>AddSong</div>
+    <div className='parent'>
+     <div>Titel-Abschnitt</div>
+     <div>Bild-Upload</div>
+     <div>Songtitle-Eingabe</div>
+     <div>mp3-Upload</div>
+     <div>Notizen/Lyrics-Eingabe</div>
+     <div>Playlist-Zuordnung</div>
+    </div>
   )
 }
 
