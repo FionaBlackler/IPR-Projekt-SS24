d01991f41df348834857759164f4aa673f725782
FAES <faes.extra@gmail.com>
2024-05-16 17:50:08 +0200

    button test

--- TripleFMusic/src/pages/MainArea/AddSong.jsx
+++ TripleFMusic/src/pages/MainArea/AddSong.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./AddSong.css"
 
 function AddSong() {
+  const [image, setImage] = useState(null)
+  const [fileName, setFileName] = useState("No selected file")
   return (
     <div className='parent'>
-     <div>Titel-Abschnitt</div>
-     <div>Bild-Upload</div>
-     <div>Songtitle-Eingabe</div>
-     <div>mp3-Upload</div>
-     <div>Notizen/Lyrics-Eingabe</div>
-     <div>Playlist-Zuordnung</div>
+
+      <div className='titleBar'>
+        <div className='titleLable'>Add a new song</div>
+        <div class="title-bar-controls">
+          <button aria-label="help">?</button>
+        </div>
+      </div>
+      
+      <div className='content'>
+        <div>mp3-Upload</div> 
+        <div>
+          <form action='' onClick={() => document.querySelector(".imageUploader").click()}>
+            <input type='file' accept='image/*' className='imageUploader' hidden/>
+           
+          </form>
+        </div>   
+        <div>
+        <input type='text' className='songtitle'/>
+        </div>
+        <input type='text' className='artist'/>
+        <div>Playlist-Zuordnung</div>
+        <div>Genre</div>
+        <input type='text' className='notesLyrics'/>
+      </div>
     </div>
   )
 }
