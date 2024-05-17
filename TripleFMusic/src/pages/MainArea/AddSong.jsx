eb088d0a5653bc3eee2b603668016a94631fa502
FAES <faes.extra@gmail.com>
2024-05-18 00:18:42 +0200

    better css

--- TripleFMusic/src/pages/MainArea/AddSong.jsx
+++ TripleFMusic/src/pages/MainArea/AddSong.jsx
@@ -1,45 +1,51 @@
-import React, { useState } from 'react'
-import "./AddSong.css"
+import React, { useState } from "react";
+import "./AddSong.css";
 
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 
-
-import rose from 'react95/dist/themes/rose'; //Thema der UI-Elemente
-import {Button} from 'react95';
+import rose from "react95/dist/themes/rose"; //Thema der UI-Elemente
+import { Button, Window, WindowHeader, WindowContent } from "react95";
+import vaporTeal from "react95/dist/themes/vaporTeal";
 
 function AddSong() {
-  const [image, setImage] = useState(null)
-  const [fileName, setFileName] = useState("No selected file")
+  const [image, setImage] = useState(null);
+  const [fileName, setFileName] = useState("No selected file");
   return (
-    <div className='parent'>
-       <ThemeProvider theme={rose}>
-      <div className='addSongtitleBar'>
-        <div className='addSongtitleLable'>Add a new song</div>
-        <div class="addSongtitle-bar-controls">
-          <button className="help" aria-label="help">?</button>
+    <ThemeProvider theme={rose}>
+      <Window className="parent">
+        <WindowHeader className="window-title">
+          <span>Add new song</span>
+          <Button className="help"> ? </Button>
+        </WindowHeader>
+
+        <div className="addSongcontent">
+          <ThemeProvider theme={vaporTeal}>
+            <Button primary>Upload mp3-file</Button>
+          </ThemeProvider>
+          <div>
+            <form
+              action=""
+              onClick={() => document.querySelector(".imageUploader").click()}
+            >
+              <input
+                type="file"
+                accept="image/*"
+                className="imageUploader"
+                hidden
+              />
+            </form>
+          </div>
+          <div>
+            <input type="text" className="songtitle" />
+          </div>
+          <input type="text" className="artist" />
+          <div className="Test">Playlist-Zuordnung</div>
+          <div>Genre</div>
+          <input type="text" className="notesLyrics" />
         </div>
-      </div>
-      
-      <div className='addSongcontent'>
-        <Button primary>Upload mp3-file</Button>
-        <Button primary>Funda ist kacke</Button>
-        <div>
-          <form action='' onClick={() => document.querySelector(".imageUploader").click()}>
-            <input type='file' accept='image/*' className='imageUploader' hidden/>
-           
-          </form>
-        </div>   
-        <div>
-        <input type='text' className='songtitle'/>
-        </div>
-        <input type='text' className='artist'/>
-        <div className='Test'>Playlist-Zuordnung</div>
-        <div>Genre</div>
-        <input type='text' className='notesLyrics'/>
-      </div>
-      </ThemeProvider>
-    </div>
-  )
+      </Window>
+    </ThemeProvider>
+  );
 }
 
-export default AddSong
+export default AddSong;
