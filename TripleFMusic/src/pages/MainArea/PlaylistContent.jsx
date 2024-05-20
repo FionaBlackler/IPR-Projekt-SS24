5238ebea3f570e6dbe28c99b56e892902cc854d8
AmyMatrix <85806803+FionaBlackler@users.noreply.github.com>
2024-05-20 03:44:21 +0200

    Updated Style

--- TripleFMusic/src/pages/MainArea/PlaylistContent.jsx
+++ TripleFMusic/src/pages/MainArea/PlaylistContent.jsx
@@ -2,22 +2,25 @@ import React from 'react';
 import { Table, TableBody, TableRow, TableDataCell } from 'react95';
 import { ThemeProvider } from 'styled-components';
 import rose from 'react95/dist/themes/rose';
+import './PlaylistContent.css';
 
 function PlaylistContent({ playlist }) {
   return (
     <ThemeProvider theme={rose}>
-      <Table>
-        <TableBody>
-          {playlist.songs.map((song, index) => (
-            <TableRow key={index}>
-              <TableDataCell>{index + 1}</TableDataCell>
-              <TableDataCell>{song.title}</TableDataCell>
-              <TableDataCell>{song.artist}</TableDataCell>
-              <TableDataCell>{song.genre}</TableDataCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <div className="playlist-content-container">
+        <Table className="playlist-table">
+          <TableBody>
+            {playlist.songs.map((song, index) => (
+              <TableRow key={index}>
+                <TableDataCell className="table-cell">{index + 1}</TableDataCell>
+                <TableDataCell className="table-cell">{song.title}</TableDataCell>
+                <TableDataCell className="table-cell">{song.artist}</TableDataCell>
+                <TableDataCell className="table-cell">{song.genre}</TableDataCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     </ThemeProvider>
   );
 }
