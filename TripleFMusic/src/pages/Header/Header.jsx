e4b55fdc95eb1baf0b3547b8be118183fabe7c5b
FAES <faes.extra@gmail.com>
2024-05-20 22:49:38 +0200

    new header

--- TripleFMusic/src/pages/Header/Header.jsx
+++ TripleFMusic/src/pages/Header/Header.jsx
@@ -1,20 +1,46 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import './Header.css'
-import logo from "./TripleF3.png"
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
+import logo from "./TripleF3_2.png";
+
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
+import rose from "react95/dist/themes/rose"; //Thema der UI-Elemente
+import { AppBar, Toolbar, Button } from "react95";
+import vaporTeal from "react95/dist/themes/vaporTeal";
 
 function Header() {
   return (
-    
-      <nav>
+    <>
+      <ThemeProvider theme={rose}>
+        <AppBar>
+          <Toolbar style={{ justifyContent: "space-between" }}>
+            <Button onClick={() => {}} style={{ fontWeight: "bold" }}>
+              <img
+                src={logo}
+                alt="TripleF Music"
+                style={{ height: "20px", marginRight: 4 }}
+              />
+              TripleF Music
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default Header;
+
+/* 
+   <nav>
         <div className='headerBody'>
           <NavLink to='/'>
             <img src={logo} className="logo" alt="TripleF Music" />
           </NavLink>
         </div>
       </nav>
-    
-  )
-}
 
-export default Header
+
+
+*/
