485f3313bd4e8a7fb572a606633177dc14a3fee3
FAES <faes.extra@gmail.com>
2024-05-20 20:33:43 +0200

    addSong

--- TripleFMusic/src/pages/MainArea/AddSong.jsx
+++ TripleFMusic/src/pages/MainArea/AddSong.jsx
@@ -4,45 +4,84 @@ import "./AddSong.css";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
 
 import rose from "react95/dist/themes/rose"; //Thema der UI-Elemente
-import { Button, Window, WindowHeader, WindowContent } from "react95";
+import {
+  Button,
+  Window,
+  WindowHeader,
+  WindowContent,
+  Separator,
+  TextInput,
+  ScrollView,
+  GroupBox,
+  Checkbox,
+} from "react95";
 import vaporTeal from "react95/dist/themes/vaporTeal";
 
 function AddSong() {
-  const [image, setImage] = useState(null);
-  const [fileName, setFileName] = useState("No selected file");
   return (
     <ThemeProvider theme={rose}>
       <Window className="parent">
         <WindowHeader className="window-title">
-          <span>Add new song</span>
+          <span className="newSong">Add new song</span>
           <Button className="help"> ? </Button>
         </WindowHeader>
 
-        <div className="addSongcontent">
-          <ThemeProvider theme={vaporTeal}>
-            <Button primary>Upload mp3-file</Button>
-          </ThemeProvider>
+        <WindowContent className="addSongcontent">
+          <Button primary>Upload mp3-file</Button>
+
+          <Separator />
+
           <div>
-            <form
-              action=""
-              onClick={() => document.querySelector(".imageUploader").click()}
-            >
-              <input
-                type="file"
-                accept="image/*"
-                className="imageUploader"
-                hidden
-              />
-            </form>
+            <Button primary>Upload song cover</Button>
           </div>
-          <div>
-            <input type="text" className="songtitle" />
+
+          <Separator />
+
+          <div className="song-title">
+            <TextInput
+              className="input-field-title"
+              placeholder="Add song title"
+              fullWidth
+            />
           </div>
-          <input type="text" className="artist" />
+
+          <Separator />
+
           <div className="Test">Playlist-Zuordnung</div>
-          <div>Genre</div>
-          <input type="text" className="notesLyrics" />
-        </div>
+
+          <GroupBox label="Genre">
+            <ScrollView>
+              <div>
+                <Checkbox name="allGenres" label="All"></Checkbox>
+              </div>
+              <div>
+                <Checkbox name="genre" label="Girl Punk"></Checkbox>
+                <Checkbox name="genre" label="Rock"></Checkbox>
+                <Checkbox name="genre" label="Pop"></Checkbox>
+                <br />
+                <Checkbox name="genre" label="Blues"></Checkbox>
+                <Checkbox name="genre" label="Country"></Checkbox>
+                <Checkbox name="genre" label="Rap"></Checkbox>
+                <br />
+                <Checkbox name="genre" label="Raggae"></Checkbox>
+                <Checkbox name="genre" label="Electronic/Dance"></Checkbox>
+                <Checkbox name="genre" label="R&B"></Checkbox>
+                <br />
+              </div>
+            </ScrollView>
+          </GroupBox>
+
+          <div className="notes">
+            <ScrollView className="notes-scroll" fullWidth>
+              <TextInput
+                className="input-field-notes"
+                placeholder="Add other notes or lyrics here"
+                multiline
+                fullWidth
+              />
+            </ScrollView>
+          </div>
+        </WindowContent>
       </Window>
     </ThemeProvider>
   );
