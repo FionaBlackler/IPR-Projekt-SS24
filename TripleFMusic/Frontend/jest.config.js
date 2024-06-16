f687dc2bbb55cb89adc6968b401ddf0961de05f1
AmyMatrix <85806803+FionaBlackler@users.noreply.github.com>
2024-06-16 19:27:46 +0200

    setting up jest for testing

--- TripleFMusic/Frontend/jest.config.js
+++ TripleFMusic/Frontend/jest.config.js
@@ -1,11 +1,24 @@
 module.exports = {
     testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+    setupFilesAfterEnv: ['/src/setupTest.js'],
     collectCoverage: true,
     coverageDirectory: 'coverage',
     coverageReporters: ['json', 'lcov', 'text', 'clover'],
     coveragePathIgnorePatterns: [
       '/node_modules/',
       '/src/index.js',
+      '/src/main.jsx',  // Added main.jsx to ignore coverage for entry point
+      '/src/pages/Images/',  // Ignoring image files
+      '/src/assets/'  // Ignoring assets folder
     ],
-  };
+    moduleNameMapper: {
+      "\\.(css|less)$": "identity-obj-proxy",  // Mocking CSS imports for Jest
+    },
+    transform: {
+      "^.+\\.jsx?$": "babel-jest",  // Transpiling JS and JSX files with Babel
+    },
+    testPathIgnorePatterns: [
+      '/node_modules/',
+      '/dist/'  // Ignoring dist folder for tests
+    ],
+};
